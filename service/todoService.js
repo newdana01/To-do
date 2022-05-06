422b4036b330926fa15b40ec7def267efae124a5
newdana01 <newdana01@gmail.com>
2022-05-06 23:03:12 +0900

    Modify: getOne Service

--- service/todoService.js
+++ service/todoService.js
@@ -1,8 +1,26 @@
 const { Todo } = require('../models/todos');
 
 const getOneTodo = async (id) => {
-  const todo = await Todo.findAll({ where: { id }, raw: true });
-  return { todo };
+  try {
+    const todo = await Todo.findOne({
+      where: { id },
+      attributes: [
+        'id',
+        'name',
+        'completed',
+        'created_at',
+        'updated_at',
+        'completed_at',
+      ],
+      raw: true,
+    });
+    if (!todo) {
+      throw new Error('Todo does not exist');
+    }
+    return todo;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 module.exports = { getOneTodo };
